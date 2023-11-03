f79d885130a6aabb09f4eff7777b53cc7f1b2561
lolrd <lolrdmen@gmail.com>
2023-11-03 15:55:50 +0600

    fix main

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import pika
 
 from core.whitelist import (national_bank_kz, cbr_forex, cbr_reestersavers, cbr_advisors,
                             cbr_trust, cbr_specdepositaries, cbr_dealers, cbr_depositaries,
-                            cbr_brokers, govkz_securities_transactions, govkz_individual_banking_transactions, scm,
-                            bafin,
+                            cbr_brokers, govkz_securities_transactions, govkz_individual_banking_transactions, bafin,
+                            scm,
                             )
 from core.blacklist import (cbr_unlicensing, cbr_warninglist, govkz_bannedbanks, govkz_banned_fin_organizations,
                             govkz_refund_organizations, govkz_bannedbanks_2level, govkz_unfairactivity_organization,
-                            asc, fsa, sfc, amf, sca, consob)
+                            consob, sca, amf, sfc, fsa, asc)
 
 logging.basicConfig(
                     filename='ETL.log',
@@ -20,34 +20,38 @@ logging.basicConfig(
                     datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='ETL', durable=True)
-
 WINDOW_BETWEEN_MESSAGES_SECONDS = 60 * 60 * 12
-
-COUNTER = 1
+WINDOW_BETWEEN_SOURCES_SECONDS = 60
 
 
 def publisher(func: callable, source_name: str) -> None:
     try:
         logging.info(f"ETL process with source <{source_name}> has been started")
-        global COUNTER
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue='ETL', durable=True)
+
+        global data_units_load_counter
         for data_unit in func:
             logging.info(data_unit)
             channel.basic_publish(exchange='',
                                   routing_key='ETL',
                                   body=data_unit
                                   )
-            COUNTER += 1
-        logging.info(f"ETL process with source <{source_name}> has been finished. Amount of dataunits: {COUNTER}")
-        time.sleep(60)
+            data_units_load_counter += 1
+        logging.info(f"ETL process with source <{source_name}> has been finished. Amount of dataunits for parsing session (this parser and before them): {data_units_load_counter}")
+        connection.close()
+        time.sleep(WINDOW_BETWEEN_SOURCES_SECONDS)
     except Exception as e:
         logging.error(f"Current resource was stopped cause of global error: {e}")
+    finally:
+        if 'connection' in locals():
+            connection.close()
 
 
 while True:
+    data_units_load_counter = 0
+
     logging.info(f"Loop of scraping has been started {datetime.now()}")
     publisher(bafin.data_unit_iterator(), "BAFIN")
     publisher(consob.data_unit_iterator(), "CONSOB")
@@ -69,10 +73,9 @@ while True:
     publisher(govkz_securities_transactions.data_unit_iterator(), "GOV KZ Реестр выданных, переоформленных лицензий на проведение банковских и иных операций и осуществление деятельности на рынке ценных бумаг")
     publisher(govkz_individual_banking_transactions.data_unit_iterator(), "GOV KZ Реестр выданных, переоформленных лицензий на осуществление отдельных видов банковских операций")
     publisher(govkz_bannedbanks.data_unit_iterator(), "GOV KZ РЕЕСТР ПРИОСТАНОВЛЕННЫХ, ЛИБО ПРЕКРАТИВШИХ ДЕЙСТВИЕ (ЛИШЕННЫХ) ЛИЦЕНЗИЙ НА ПРОВЕДЕНИЕ БАНКОВСКИХ И ИНЫХ ОПЕРАЦИЙ, ОСУЩЕСТВЛЯЕМЫХ БАНКАМИ")
-    publisher(govkz_bannedbanks_2level.data_unit_iterator(), "GOV KZ РЕЕСТР ПРИОСТАНОВЛЕННЫХ, ЛИБО ПРЕКРАТИВШИХ ДЕЙСТВИЕ (ЛИШЕННЫХ) ЛИЦЕНЗИЙ НА ПРОВЕДЕНИЕ БАНКОВСКИХ И ИНЫХ ОПЕРАЦИЙ, ОСУЩЕСТВЛЯЕМЫХ БАНКАМИ")
     publisher(govkz_banned_fin_organizations.data_unit_iterator(), "GOV KZ Реестр приостановленных, либо прекративших действие (лишенных) лицензий на осуществление отдельных видов банковских операций")
     publisher(govkz_refund_organizations.data_unit_iterator(), "GOV KZ Реестр прекративших действие лицензий организаций, осуществлявших отдельные виды банковских операций в связи с добровольным возвратом.")
-    publisher(govkz_refund_organizations.data_unit_iterator(), "GOV KZ Реестр прекративших действие лицензий банков второго уровня в связи с добровольным возвратом")
+    publisher(govkz_bannedbanks_2level.data_unit_iterator(), "GOV KZ Реестр прекративших действие лицензий банков второго уровня в связи с добровольным возвратом")
     publisher(govkz_unfairactivity_organization.data_unit_iterator(), "GOV KZ Список организаций, имеющих признаки недобросовестной деятельности")
     publisher(cbr_unlicensing.data_unit_iterator(), "ЦБ РФ Реестр аннулированных лицензий профессиональных участников рынка ценных бумаг")
     publisher(cbr_warninglist.data_unit_iterator(), "ЦБ РФ Список компаний с выявленными признаками нелегальной деятельности на финансовом рынке")
