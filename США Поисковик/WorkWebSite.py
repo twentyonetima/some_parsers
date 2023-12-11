971c8073c8a36293b25bc2521e626b9e59482439
Тимофей Петров <twentyonetima@mail.ru>
2023-12-11 06:57:03 +0000

    Update WorkWebSite.py

--- США Поисковик/WorkWebSite.py
+++ США Поисковик/WorkWebSite.py
@@ -1,25 +1,29 @@
 import time
-from googletrans import Translator, constants
-from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
+from googletrans import Translator
+from urllib.parse import urljoin
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import SaveHdd
 
 
-def change_using(driver, element_name, ):
+def change_using(soup, element_name):
     """
     Метод перехода по ссылкам
     :param element_name: имя селектора
-    :param driver:
+    :param soup: BeautifulSoup object
     :return:
     """
-
-    find_elements = driver.find_elements(By.CSS_SELECTOR, element_name)
-
+    find_elements = soup.select(element_name)
     return find_elements
 
 
-def parsing(url, driver, type_list):
+def parsing(url, type_list, soup):
     """
     Функция тестирования  и запуска парсера
-    :param drivers:
+    :param soup: BeautifulSoup object
+    :param type_list: Type of the list
     :return:
     """
     translator = Translator()
@@ -27,50 +31,66 @@ def parsing(url, driver, type_list):
     json_dictionary = {}
     key = []
     value = []
-    all_dictonary = []
-    data_published_link = []
+    all_dictionary = []
     data_links = []
-    page_number = 0
     count_organization = 0
-    # click_button(driver, ".cookie-btn", 3)
-    time.sleep(5)
-    list_data = change_using(driver, ".views-field-field-display-title ")
 
-    for i in list_data:
-        data_published_link.append(i.find_elements(By.TAG_NAME, 'a'))
+    list_data = soup.select('.views-field-field-display-title')
 
-    for j in range(len(data_published_link)):
-        if j != 0:
-            data_links.append(data_published_link[j][0].get_attribute("href"))
+    for cell in list_data:
+        # Extract the link and name from each cell
+        link = cell.find('a')
 
-    while page_number < len(data_links):       #10 для тестов
-        print(data_links[page_number])
-        driver.get(data_links[page_number])
-        count_organization += 1
+        # Skip this cell if there's no link
+        if link is None:
+            continue
+
+        # Construct the absolute URL
+        absolute_url = urljoin(url, link.get("href"))
+
+        # Make the request using the absolute URL
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(absolute_url, headers=headers)
+        time.sleep(2)
+        sub_soup = BeautifulSoup(response.text, 'html.parser')
+
         print("Number organization", count_organization)
-        value_items = driver.find_elements(By.CSS_SELECTOR, '.margin-20')
-        name_organization = driver.find_element(By.CSS_SELECTOR, '.article-title')
-        key.append("name")
-        value.append(name_organization.text)
 
+        # Find the name_organization element, handle None case
+        name_organization = sub_soup.select_one('.article-title')
+        if name_organization:
+            key.append("name")
+            value.append(name_organization.text)
+        else:
+            value.append("Name not found")
+
+        value_items = sub_soup.select('.margin-20')
+        print()
         for k in range(len(value_items)):
             template = value_items[k].text
+            # print("Template:", template)
             template = template.split("\n")
 
             for x in range(len(template)):
+                template[x] = template[x].replace('\r', '').replace('\xa0', '').replace('\xa0\r', '').replace(' \\xa', '').replace('\xa0\xa0 \\xa0', '')
+
                 items = (template[x].split(":"))
 
-                if x != 0 and x <= 1:
+                # print("Items:", items, "Lens:", len(items[0]))
+
+                if x != 0 and x <= 2 and len(items[0]) > 0:
                     if len(items) == 1 and len(template[x - 1].split(":")) == 1:
-                        items_adress = (template[x - 1] + " " + template[x])
+                        items_address = (template[x - 1] + " " + template[x])
                         key.append("legal_entity_address")
-                        translation = translator.translate(items_adress, dest='ru')
+                        translation = translator.translate(items_address, dest='ru')
                         print(translation.text)
                         value.append(translation.text)
                     else:
-                        items_adress = (template[x - 1])
+                        items_address = (template[x - 1])
                         key.append("legal_entity_address")
-                        translation = translator.translate(items_adress, dest='ru')
+                        translation = translator.translate(items_address, dest='ru')
                         print(translation.text)
                         value.append(translation.text)
 
@@ -85,9 +105,9 @@ def parsing(url, driver, type_list):
                     elif items[0] == "Website":
                         key.append("links")
                         if items[1] == ' http':
-                            value.append(['http' + items[2], ])
+                            value.append(['http:' + items[2], ])
                         elif items[1] == ' https':
-                            value.append(['https' + items[2], ])
+                            value.append(['https:' + items[2], ])
                         else:
                             value.append([items[1], ])
 
@@ -104,18 +124,8 @@ def parsing(url, driver, type_list):
                         else:
                             value.append(items[1])
 
-
-
-
         count = 1
         print("Len Key", len(key))
-        # print("Len Value", len(value))
-        # chek = len(key)
-
-        # for keys in key:
-        #     print("Key:", keys)
-        # for values in value:
-        #     print("Values:", values)
 
         for s in range(len(key)):
             if count != len(key) + 1:
@@ -123,15 +133,15 @@ def parsing(url, driver, type_list):
                 json_dictionary[key[s]] = value[s]
                 if count == len(key) + 1:
                     json_dictionary['type'] = type_list
-                    json_dictionary['source'] = \
-                        'https://www.sec.gov/enforce/public-alerts/' \
-                        'impersonators-genuine-firms/12-west-capital-management'
+                    json_dictionary['source'] = absolute_url
                     json_dictionary['Country'] = "United States"
-                    all_dictonary.append(json_dictionary)
+                    print(json_dictionary)
+                    all_dictionary.append(json_dictionary)
                     json_dictionary = {}
                     count = 0
                     value = []
                     key = []
-        page_number += 1
+        count_organization += 1
+        # page_number += 1
 
-    return all_dictonary
+    return all_dictionary
