d3433c2ce73324fc35f02af0312ed1ee309af6cc
Тимофей Петров <twentyonetima@mail.ru>
2023-12-12 07:38:51 +0000

    Update WorkWebSite.py

--- США Поисковик/WorkWebSite.py
+++ США Поисковик/WorkWebSite.py
@@ -145,3 +145,32 @@ def parsing(url, type_list, soup):
         # page_number += 1
 
     return all_dictionary
+
+
+if __name__ == "__main__":
+    # region parameter Browser
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--headless=new")
+    driver = webdriver.Chrome()
+    url = "https://www.sec.gov/enforce/public-alerts"
+    driver.get(url)
+
+    # Wait for a few seconds to let the JavaScript load the content
+    time.sleep(5)
+
+    # Get the page source after JavaScript execution
+    page_source = driver.page_source
+
+    # Close the browser
+    driver.quit()
+    # endregion
+
+    # Use BeautifulSoup for parsing the page source
+    soup = BeautifulSoup(page_source, 'html.parser')
+
+    # Call the parsing function with the URL, type_list, and soup
+    test = parsing(url, "black_list", soup)
+
+    # Save the result to a JSON file
+    # save = SaveHdd.save_json(test)
