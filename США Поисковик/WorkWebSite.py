5e7978e2e9770195679311d6993c98b4b4f37a27
Тимофей Петров <twentyonetima@mail.ru>
2023-12-12 08:16:29 +0000

    add timeout=20 for all requests

--- США Поисковик/WorkWebSite.py
+++ США Поисковик/WorkWebSite.py
@@ -52,7 +52,7 @@ def parsing(url, type_list, soup):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
-        response = requests.get(absolute_url, headers=headers)
+        response = requests.get(absolute_url, headers=headers, timeout=20)
         time.sleep(2)
         sub_soup = BeautifulSoup(response.text, 'html.parser')
 
